86c106315ef4cc600c5f5cf59b101bbb0cc0cdc2
seanthecod3r <sean.celli@yahoo.com>
2023-10-17 20:08:22 -0700

    Basic express/mongodb server skeleton

---
+++ Backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = 3001;
+
+// Connect to MongoDB
+mongoose
+  .connect("mongodb://localhost:27017/mydatabase", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
