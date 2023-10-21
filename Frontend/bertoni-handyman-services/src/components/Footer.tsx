cf34bb4127c8791d5dbedb5338dd88276936ed65
mariellemcbride <mariellemcbride@ymail.com>
2023-10-21 15:07:07 -0700

    Update Footer.tsx

--- Frontend/bertoni-handyman-services/src/components/Footer.tsx
+++ Frontend/bertoni-handyman-services/src/components/Footer.tsx
@@ -1,20 +1,13 @@
 import React from "react";
 import FullSectionLayout from "../layouts/FullSectionLayout";
 
-const footerStyle = {
-  fontFamily: 'Inter',
-  fontSize: '20px',
-  fontWeight: 400,
-  lineHeight: '24px',
-  letterSpacing: '0em',
-  textAlign: 'left',
-};
-
 const Footer = () => {
   return (
-    <footer style={footerStyle}>
-      @ 2023 Bertoni Handyman Services. All rights reserved.
-    </footer>
+    <FullSectionLayout>
+      <div className="font-inter text-20 text-gray-400 leading-24 tracking-0">
+      &copy; 2023 Bertoni Handyman Services. All rights reserved.
+      </div>
+      </FullSectionLayout>
   );
 }
 
