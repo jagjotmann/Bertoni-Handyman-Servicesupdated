6c271eac6813b7dbc2568f9594a9dc955bfb8a3a
Ahmed Osman <ahmedaosman00@gmail.com>
2023-11-04 13:34:22 -0700

    Creating the sent form

--- Frontend/bertoni-handyman-services/src/pages/GetAQuote.jsx
+++ Frontend/bertoni-handyman-services/src/pages/GetAQuote.jsx
@@ -8,14 +8,62 @@ const QuoteForm = () => {
   const [preferredFinishDate, setPreferredFinishDate] = useState("");
   const [errors, setErrors] = useState({});
   const [specifications, setSpecifications] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [setIsSubmitted] = useState(false);
 
-  const handleSubmit = (event) => {
+  const validateForm = () => {
+    const newErrors = {};
+    if (!firstName.trim()) newErrors.firstName = "First Name cannot be empty";
+    if (!lastName.trim()) newErrors.lastName = "Last Name cannot be empty";
+    if (!email.trim()) newErrors.email = "Email cannot be empty";
+    if (!specifications.trim())
+      newErrors.specifications = "Specifications cannot be empty";
+    return newErrors;
+  };
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    let newErrors = {};
-    if (!firstName) newErrors.firstName = "First Name cannot be empty";
-    if (!lastName) newErrors.lastName = "Last Name cannot be empty";
-    if (!email) newErrors.email = "Email cannot be empty";
-    setErrors(newErrors);
+    if (isSubmitting) return;
+
+    let formErrors = validateForm();
+    setErrors(formErrors);
+
+    if (Object.keys(formErrors).length === 0) {
+      setIsSubmitting(true);
+      // Mock API call - replace with actual API call
+      try {
+        const response = await sendFormDataToServer({
+          firstName,
+          lastName,
+          email,
+          phoneNumber,
+          preferredFinishDate,
+          specifications,
+        });
+        // Handle response
+        console.log(response);
+        // Reset form fields
+        setFirstName("");
+        setLastName("");
+        setEmail("");
+        setPhoneNumber("");
+        setPreferredFinishDate("");
+        setSpecifications("");
+        setIsSubmitted(true);
+      } catch (error) {
+        // Handle error
+        console.error(error);
+        setErrors({ submit: "Failed to submit the form. Please try again." });
+      }
+      setIsSubmitting(false);
+    }
+  };
+
+  const sendFormDataToServer = (formData) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({ status: "success", ...formData });
+      }, 2000);
+    });
   };
 
   const styles = {
@@ -70,19 +118,31 @@ const QuoteForm = () => {
         "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)", // Example for shadow-custom-shadow, adjust your actual shadow
       transition: "transform 0.2s",
     },
+
+    sentContainer: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      maxWidth: "600px",
+      margin: "0 auto",
+      padding: "40px 0",
+    },
+    signInLink: {
+      textDecoration: "underline",
+      color: "#ffa500",
+    },
   };
 
-  return (
-    <div style={styles.container}>
+  const renderForm = () => (
+    <>
       <div style={styles.title}>Get a Quote</div>
       <div style={{ marginBottom: "20px" }}>
-        Already have an account? {""}
+        Already have an account?{" "}
         <a href="/signin" style={{ textDecoration: "underline" }}>
           Sign In
         </a>
         &nbsp; here.
       </div>
-
       <form style={styles.form} onSubmit={handleSubmit}>
         <div style={styles.row}>
           <input
@@ -98,7 +158,7 @@ const QuoteForm = () => {
             onBlur={() =>
               setErrors({
                 ...errors,
-                firstName: firstName ? "" : "First Name cannot be empty",
+                firstName: firstName.trim() ? "" : "First Name cannot be empty",
               })
             }
           />
@@ -115,7 +175,7 @@ const QuoteForm = () => {
             onBlur={() =>
               setErrors({
                 ...errors,
-                lastName: lastName ? "" : "Last Name cannot be empty",
+                lastName: lastName.trim() ? "" : "Last Name cannot be empty",
               })
             }
           />
@@ -144,7 +204,7 @@ const QuoteForm = () => {
             onBlur={() =>
               setErrors({
                 ...errors,
-                email: email ? "" : "Email cannot be empty",
+                email: email.trim() ? "" : "Email cannot be empty",
               })
             }
           />
@@ -166,9 +226,9 @@ const QuoteForm = () => {
             onBlur={() =>
               setErrors({
                 ...errors,
-                specifications: specifications
+                specifications: specifications.trim()
                   ? ""
-                  : "Specifications cannot be empty",
+                  : "Specifications and Requirements cannot be empty",
               })
             }
           ></textarea>
@@ -189,13 +249,38 @@ const QuoteForm = () => {
         </div>
         <button
           type="submit"
-          className="bg-orange-500 text-black text-xl sm:text-2xl md:text-2xl lg:text-3xl px-4 sm:px-5 md:px-6 lg:px-7 py-3 sm:py-3 md:py-4 lg:py-5 font-bold mr-4 sm:mr-6 md:mr-8 lg:mr-10 focus:outline-none focus:ring-2 focus:ring-orange-400 hover:scale-105 shadow-custom-shadow transition-transform"
-          style={{ borderRadius: "10px" }}
+          disabled={isSubmitting} // Disable button during form submission
+          style={{ ...styles.button, opacity: isSubmitting ? 0.5 : 1 }}
         >
-          {" "}
-          Send{" "}
+          {isSubmitting ? "Sending..." : "Send"}
         </button>
+        {errors.submit && <p style={{ color: "red" }}>{errors.submit}</p>}
       </form>
+    </>
+  );
+
+  return (
+    <div style={styles.container}>
+      {isSubmitting ? (
+        // Show success message after form submission
+        <div style={styles.sentContainer}>
+          <h2 class="title">Sent!</h2>
+          <p style={{ color: "orange", fontSize: "16px" }}>
+            Thanks for submitting a Quote Request!
+          </p>
+          <p>We have sent you an email with your Quote Number.</p>
+          <p>
+            You can use your Quote Number to sign into your account to view your
+            Quote's status.
+          </p>
+          <a href="/signin" style={styles.signInLink}>
+            Sign in
+          </a>
+        </div>
+      ) : (
+        // Show form
+        renderForm()
+      )}
     </div>
   );
 };
