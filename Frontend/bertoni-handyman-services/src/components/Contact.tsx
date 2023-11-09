37ccc42ddbadbcf116eb8e63453c21d91f927c24
Jagjot Mann <jagjot_mann@Jagjots-MBP.attlocal.net>
2023-11-09 00:52:53 -0800

    changes

--- Frontend/bertoni-handyman-services/src/components/Contact.tsx
+++ Frontend/bertoni-handyman-services/src/components/Contact.tsx
@@ -2,18 +2,39 @@ import React from "react";
 import PaddingSectionLayout from "../layouts/PaddingSectionLayout";
 
 const Contact = () => {
+  const customStyles = {
+    container: {
+      maxWidth: '760px', // Adjust this width to match your mockup's width
+    },
+    header: {
+      fontSize: '24px', // Adjust font size as needed
+      fontWeight: '600', // Adjust font weight as needed: '400' for regular, '600' for semi-bold
+      color: '#333', // Adjust the color to match your mockup
+      marginBottom: '16px', // Adjust the space between the header and the email
+    },
+    text: {
+      fontSize: '20px', // Adjust font size as needed
+      color: '#555', // Adjust the color to match your mockup
+      marginBottom: '16px', // Adjust the space between elements
+    },
+    serving: {
+      fontSize: '20px', // Adjust font size as needed
+      fontWeight: '600', // Adjust for semi-bold
+      textTransform: 'uppercase', // Makes text uppercase
+      letterSpacing: '1px', // Adjust letter spacing as needed
+      color: '#333', // Adjust the color to match your mockup
+    }
+  };
+
   return (
     <PaddingSectionLayout>
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="text-center max-w-xl mx-auto"> 
-          <h2 className="text-xl font-semibold text-gray-800">Feel free to contact at:</h2>
-          <p className="text-lg text-gray-700">Bertoni.Sean@gmail.com</p>
-          <p className="text-lg text-gray-700">(916) 508-1742</p>
-          <h3 className="text-lg font-semibold text-gray-800 uppercase tracking-wider">Proudly Serving</h3>
-          <h3 className="text-lg text-gray-700">Sacramento and Surrounding Counties</h3>
-        </div>
+      <div style={customStyles.container} className="mx-auto px-4 text-center">
+        <h2 style={customStyles.header}>Feel free to contact at:</h2>
+        <p style={customStyles.text}>Bertoni.Sean@gmail.com</p>
+        <p style={customStyles.text}>(916) 508-1742</p>
+        <h3 style={customStyles.serving}>Proudly Serving</h3>
+        <h3 style={customStyles.text}>Sacramento and Surrounding Counties</h3>
       </div>
-
     </PaddingSectionLayout>
   );
 };
