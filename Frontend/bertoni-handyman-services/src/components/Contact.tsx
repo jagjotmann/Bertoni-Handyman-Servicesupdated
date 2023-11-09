687762c7fd969f658e6fc1c04d2560f58da834a5
Jagjot Mann <jagjot_mann@Jagjots-MBP.attlocal.net>
2023-11-08 23:59:15 -0800

    changes

--- Frontend/bertoni-handyman-services/src/components/Contact.tsx
+++ Frontend/bertoni-handyman-services/src/components/Contact.tsx
@@ -5,12 +5,18 @@ const Contact = () => {
   return (
     <PaddingSectionLayout>
       <div className="contact-container">
+      <div className="contact-header">
         <h2>Feel free to contact at:</h2>
+        </div>
+        <div className="contact-info">
         <p>Bertoni.Sean@gmail.com</p>
         <p>(916) 508-1742</p>
+        </div>
+        <div className="contact-serving">
         <h3>PROUDLY SERVING</h3>
         <h3>SACRAMENTO AND</h3>
         <h3>SURROUNDING COUNTIES</h3>
+      </div>
       </div>
     </PaddingSectionLayout>
   );
