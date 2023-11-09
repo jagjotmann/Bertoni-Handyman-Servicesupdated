13531dcbbe579c81302b410d2ddbb5239b3610cd
Jagjot Mann <jagjot_mann@Jagjots-MBP.attlocal.net>
2023-11-09 01:05:07 -0800

    changes

--- Frontend/bertoni-handyman-services/src/components/Contact.tsx
+++ Frontend/bertoni-handyman-services/src/components/Contact.tsx
@@ -5,8 +5,8 @@ const Contact = () => {
   return (
     <PaddingSectionLayout>
       <div 
-        className="mx-auto px-4 text-center" 
-        style={{ maxWidth: '760px' }} // Adjust this width to match your mockup's width
+        className="mx-auto px-4" 
+        style={{ maxWidth: '1024px' }} // Adjust this width to match your mockup's width
       >
         <h2 
           style={{ 
@@ -14,6 +14,7 @@ const Contact = () => {
             fontWeight: '600', // Adjust font weight as needed: '400' for regular, '600' for semi-bold
             color: '#333', // Adjust the color to match your mockup
             marginBottom: '16px', // Adjust the space between the header and the email
+            textAlign: 'left', // Align text to the left
           }}
         >
           Feel free to contact at:
@@ -23,6 +24,7 @@ const Contact = () => {
             fontSize: '20px', // Adjust font size as needed
             color: '#555', // Adjust the color to match your mockup
             marginBottom: '16px', // Adjust the space between elements
+            textAlign: 'left', // Align text to the left
           }}
         >
           Bertoni.Sean@gmail.com
@@ -32,6 +34,7 @@ const Contact = () => {
             fontSize: '20px', // Adjust font size as needed
             color: '#555', // Adjust the color to match your mockup
             marginBottom: '16px', // Adjust the space between elements
+            textAlign: 'left', // Align text to the left
           }}
         >
           (916) 508-1742
@@ -43,6 +46,7 @@ const Contact = () => {
             textTransform: 'uppercase', // Makes text uppercase
             letterSpacing: '1px', // Adjust letter spacing as needed
             color: '#333', // Adjust the color to match your mockup
+            textAlign: 'left', // Align text to the left
           }}
         >
           Proudly Serving
@@ -51,6 +55,7 @@ const Contact = () => {
           style={{ 
             fontSize: '20px', // Adjust font size as needed
             color: '#555', // Adjust the color to match your mockup
+            textAlign: 'left', // Align text to the left
           }}
         >
           Sacramento and Surrounding Counties
