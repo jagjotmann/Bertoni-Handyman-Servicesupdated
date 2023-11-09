edf11943cb49460b1ca9c978779ee6b5e785af90
Jagjot Mann <jagjot_mann@Jagjots-MBP.attlocal.net>
2023-11-09 01:13:42 -0800

    changes

--- Frontend/bertoni-handyman-services/src/components/Contact.tsx
+++ Frontend/bertoni-handyman-services/src/components/Contact.tsx
@@ -4,62 +4,28 @@ import PaddingSectionLayout from "../layouts/PaddingSectionLayout";
 const Contact = () => {
   return (
     <PaddingSectionLayout>
-      <div 
-        className="mx-auto px-4" 
-        style={{ maxWidth: '1024px' }} // Adjust this width to match your mockup's width
-      >
-        <h2 
-          style={{ 
-            fontSize: '24px', // Adjust font size as needed
-            fontWeight: '600', // Adjust font weight as needed: '400' for regular, '600' for semi-bold
-            color: '#333', // Adjust the color to match your mockup
-            marginBottom: '16px', // Adjust the space between the header and the email
-            textAlign: 'left', // Align text to the left
-          }}
-        >
-          Feel free to contact at:
-        </h2>
-        <p 
-          style={{ 
-            fontSize: '20px', // Adjust font size as needed
-            color: '#555', // Adjust the color to match your mockup
-            marginBottom: '16px', // Adjust the space between elements
-            textAlign: 'left', // Align text to the left
-          }}
-        >
-          Bertoni.Sean@gmail.com
-        </p>
-        <p 
-          style={{ 
-            fontSize: '20px', // Adjust font size as needed
-            color: '#555', // Adjust the color to match your mockup
-            marginBottom: '16px', // Adjust the space between elements
-            textAlign: 'left', // Align text to the left
-          }}
-        >
-          (916) 508-1742
-        </p>
-        <h3 
-          style={{ 
-            fontSize: '20px', // Adjust font size as needed
-            fontWeight: '600', // Adjust for semi-bold
-            textTransform: 'uppercase', // Makes text uppercase
-            letterSpacing: '1px', // Adjust letter spacing as needed
-            color: '#333', // Adjust the color to match your mockup
-            textAlign: 'left', // Align text to the left
-          }}
-        >
-          Proudly Serving Sacramento and Surrounding Counties
-        </h3>
-        <h3 
-          style={{ 
-            fontSize: '20px', // Adjust font size as needed
-            color: '#555', // Adjust the color to match your mockup
-            textAlign: 'right', // Align text to the left
-          }}
-      
-          >
-        </h3>
+      <div className="container mx-auto px-4">
+        <div className="flex justify-between items-center" style={{ maxWidth: '1024px' }}> {/* Adjust this width to match your mockup's width */}
+          <div>
+            <h2 style={{ fontSize: '24px', fontWeight: '600', color: '#333' }}>
+              Feel free to contact at:
+            </h2>
+            <p style={{ fontSize: '20px', color: '#555' }}>
+              Bertoni.Sean@gmail.com
+            </p>
+            <p style={{ fontSize: '20px', color: '#555' }}>
+              (916) 508-1742
+            </p>
+          </div>
+          <div className="text-right">
+            <h3 style={{ fontSize: '20px', fontWeight: '600', textTransform: 'uppercase', letterSpacing: '1px', color: '#333' }}>
+              Proudly Serving
+            </h3>
+            <h3 style={{ fontSize: '20px', color: '#555' }}>
+              Sacramento and Surrounding Counties
+            </h3>
+          </div>
+        </div>
       </div>
     </PaddingSectionLayout>
   );
