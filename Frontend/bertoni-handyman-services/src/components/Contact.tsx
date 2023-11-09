7d7d447f7ad1d76867497bad9ea3c1a21990f2e7
Jagjot Mann <jagjot_mann@Jagjots-MBP.attlocal.net>
2023-11-09 01:36:36 -0800

    change

--- Frontend/bertoni-handyman-services/src/components/Contact.tsx
+++ Frontend/bertoni-handyman-services/src/components/Contact.tsx
@@ -2,27 +2,25 @@ import React from "react";
 import PaddingSectionLayout from "../layouts/PaddingSectionLayout";
 
 const Contact = () => {
+  const textStyle = {
+    fontSize: '20px', // Adjust font size as needed
+    fontWeight: '600', // Adjust font weight to match the mockup, '600' for semi-bold
+    color: '#333', // Adjust the color to match your mockup
+  };
+
   return (
     <PaddingSectionLayout>
-      <div className="container mx-auto px-4">
-        <div className="flex justify-between items-start" style={{ maxWidth: '1024px' }}> {/* Adjust this width to match your mockup's width */}
+      <div className="container mx-auto px-4" style={{ maxWidth: '1024px' }}>
+        <div className="flex justify-between items-start">
           <div>
-            <h2 style={{ fontSize: '24px', fontWeight: '600', color: '#333' }}>
-              Feel free to contact at:
-            </h2>
-            <p style={{ fontSize: '20px', color: '#555' }}>
-              Bertoni.Sean@gmail.com
-            </p>
-            <p style={{ fontSize: '20px', color: '#555' }}>
-              (916) 508-1742
-            </p>
+            <p style={textStyle}>Feel free to contact at:</p>
+            <p style={textStyle}>Bertoni.Sean@gmail.com</p>
+            <p style={textStyle}>(916) 508-1742</p>
           </div>
-          <div className="text-right" style={{ fontSize: '20px', color: '#555' }}>
-            <p style={{ fontWeight: '600', textTransform: 'uppercase' }}>
-              Proudly Serving
-            </p>
-            <p>Sacramento and</p>
-            <p>Surrounding Counties</p>
+          <div className="text-right" style={{ maxWidth: '50%' }}> {/* This ensures the text does not stretch beyond half the container */}
+            <p style={textStyle}>PROUDLY SERVING</p>
+            <p style={textStyle}>SACRAMENTO AND</p>
+            <p style={textStyle}>SURROUNDING COUNTIES</p>
           </div>
         </div>
       </div>
