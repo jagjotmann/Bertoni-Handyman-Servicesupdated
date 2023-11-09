39f7edefc51ceacaebc97fe449915cb07b307e81
Jagjot Mann <jagjot_mann@Jagjots-MBP.attlocal.net>
2023-11-09 00:06:23 -0800

    change

--- Frontend/bertoni-handyman-services/src/components/Contact.tsx
+++ Frontend/bertoni-handyman-services/src/components/Contact.tsx
@@ -4,19 +4,14 @@ import PaddingSectionLayout from "../layouts/PaddingSectionLayout";
 const Contact = () => {
   return (
     <PaddingSectionLayout>
-      <div className="contact-container">
-      <div className="contact-header">
-        <h2>Feel free to contact at:</h2>
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="text-center">
+          <h2 className="text-2xl font-bold text-gray-800">Feel free to contact at:</h2>
+          <p className="mt-2 text-lg text-gray-600">Bertoni.Sean@gmail.com</p>
+          <p className="text-lg text-gray-600">(916) 508-1742</p>
+          <h3 className="mt-4 text-xl font-bold text-gray-800 uppercase">Proudly Serving</h3>
+          <h3 className="text-lg text-gray-600">Sacramento and Surrounding Counties</h3>
         </div>
-        <div className="contact-info">
-        <p>Bertoni.Sean@gmail.com</p>
-        <p>(916) 508-1742</p>
-        </div>
-        <div className="contact-serving">
-        <h3>PROUDLY SERVING</h3>
-        <h3>SACRAMENTO AND</h3>
-        <h3>SURROUNDING COUNTIES</h3>
-      </div>
       </div>
     </PaddingSectionLayout>
   );
