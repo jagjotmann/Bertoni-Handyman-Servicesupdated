d2be852f5f6ce937036684a56c643c411f3737a4
Jagjot Mann <jagjot_mann@Jagjots-MBP.attlocal.net>
2023-11-09 00:33:51 -0800

    changes

--- Frontend/bertoni-handyman-services/src/components/Contact.tsx
+++ Frontend/bertoni-handyman-services/src/components/Contact.tsx
@@ -5,14 +5,15 @@ const Contact = () => {
   return (
     <PaddingSectionLayout>
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="text-center max-w-2xl mx-auto"> {/* Adjust this max-width to match the design */}
-          <h2 className="text-xl font-semibold text-gray-900 mb-3">Feel free to contact at:</h2>
+        <div className="text-center max-w-xl mx-auto"> 
+          <h2 className="text-xl font-semibold text-gray-800">Feel free to contact at:</h2>
           <p className="text-lg text-gray-700">Bertoni.Sean@gmail.com</p>
-          <p className="text-lg text-gray-700 mb-3">(916) 508-1742</p>
-          <h3 className="text-lg font-semibold text-gray-900 uppercase tracking-wider">Proudly Serving</h3>
+          <p className="text-lg text-gray-700">(916) 508-1742</p>
+          <h3 className="text-lg font-semibold text-gray-800 uppercase tracking-wider">Proudly Serving</h3>
           <h3 className="text-lg text-gray-700">Sacramento and Surrounding Counties</h3>
         </div>
       </div>
+
     </PaddingSectionLayout>
   );
 };
