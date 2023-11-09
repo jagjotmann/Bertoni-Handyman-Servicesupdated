08396c95c54770a752505d42d269749c60c29cd3
Jagjot Mann <jagjot_mann@Jagjots-MBP.attlocal.net>
2023-11-09 00:18:40 -0800

    change

--- Frontend/bertoni-handyman-services/src/components/Contact.tsx
+++ Frontend/bertoni-handyman-services/src/components/Contact.tsx
@@ -5,12 +5,12 @@ const Contact = () => {
   return (
     <PaddingSectionLayout>
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="text-center max-w-lg mx-auto">
-          <h2 className="text-2xl font-bold text-gray-800">Feel free to contact at:</h2>
-          <p className="mt-2 text-lg text-gray-600">Bertoni.Sean@gmail.com</p>
-          <p className="text-lg text-gray-600">(916) 508-1742</p>
-          <h3 className="mt-4 text-xl font-bold text-gray-800 uppercase">Proudly Serving</h3>
-          <h3 className="text-lg text-gray-600">Sacramento and Surrounding Counties</h3>
+        <div className="text-center max-w-2xl mx-auto"> {/* Adjust this max-width to match the design */}
+          <h2 className="text-xl font-semibold text-gray-900 mb-3">Feel free to contact at:</h2>
+          <p className="text-lg text-gray-700">Bertoni.Sean@gmail.com</p>
+          <p className="text-lg text-gray-700 mb-3">(916) 508-1742</p>
+          <h3 className="text-lg font-semibold text-gray-900 uppercase tracking-wider">Proudly Serving</h3>
+          <h3 className="text-lg text-gray-700">Sacramento and Surrounding Counties</h3>
         </div>
       </div>
     </PaddingSectionLayout>
