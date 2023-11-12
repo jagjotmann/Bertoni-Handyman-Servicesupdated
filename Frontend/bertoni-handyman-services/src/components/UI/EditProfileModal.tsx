f805ca9d7d7d9461302dcab76ee76c9da8d61482
Ahmed Osman <ahmedaosman00@gmail.com>
2023-11-12 13:44:09 -0800

    Completing the edit profile modal

--- Frontend/bertoni-handyman-services/src/components/UI/EditProfileModal.tsx
+++ Frontend/bertoni-handyman-services/src/components/UI/EditProfileModal.tsx
@@ -1,24 +1,63 @@
-import React from "react";
+// EditProfileModal.tsx
+import React, { useState } from "react";
 
 interface EditProfileModalProps {
   onClose: () => void;
 }
+
 const EditProfileModal: React.FC<EditProfileModalProps> = ({ onClose }) => {
-  // Modal content and form submission logic go here
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const saveProfile = () => {
+    // Implement save logic
+    console.log(name, email, phoneNumber);
+    onClose(); // Close modal after save
+  };
 
   return (
-    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full">
-      <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
-        {/* Close button */}
-        <div className="flex justify-end">
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+      <div className="bg-white p-4 rounded-lg">
+        <div className="flex justify-between items-center">
+          <h2 className="text-xl font-bold">Edit Profile</h2>
           <button onClick={onClose}>X</button>
         </div>
-
-        {/* Form fields */}
-        <form>
-          {/* Form fields go here */}
-          <button type="submit">Save Changes</button>
-        </form>
+        <div className="my-4">
+          <label>Name</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="border p-2 rounded w-full"
+          />
+        </div>
+        <div className="my-4">
+          <label>Email</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="border p-2 rounded w-full"
+          />
+        </div>
+        <div className="my-4">
+          <label>Phone Number</label>
+          <input
+            type="tel"
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            className="border p-2 rounded w-full"
+          />
+        </div>
+        <div className="flex justify-end">
+          <button
+            onClick={saveProfile}
+            className="bg-black text-white p-2 rounded"
+          >
+            Save
+          </button>
+        </div>
       </div>
     </div>
   );
