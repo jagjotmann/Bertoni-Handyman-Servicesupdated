2b202d632cc69ce2397048911b1f22ddbab15df7
Ahmed Osman <ahmedaosman00@gmail.com>
2023-11-25 13:08:07 -0800

    Completing the UI of the admin page

--- Frontend/bertoni-handyman-services/src/pages/AdminPage.tsx
+++ Frontend/bertoni-handyman-services/src/pages/AdminPage.tsx
@@ -1,7 +1,7 @@
 // AdminPage.tsx
 import React from "react";
-import QuoteRequests from './QuoteRequests';
-import Management from './Management';
+import QuoteRequests from "./QuoteRequests";
+import Management from "./Management";
 import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { Disclosure } from "@headlessui/react";
@@ -14,6 +14,10 @@ import {
   FaBuilding,
   FaChartBar,
 } from "react-icons/fa";
+
+import { IoBriefcaseOutline } from "react-icons/io5";
+import { LuClock } from "react-icons/lu";
+import { CiBadgeDollar } from "react-icons/ci";
 
 function AdminPage() {
   // State to track which tab is currently selected
@@ -229,8 +233,130 @@ function AdminPage() {
       </aside>
       {/* Main content area */}
       <main className="flex-1 w-full overflow-x-hidden md:pl-60">
-        {/* Conditional rendering of components based on the selected tab */}
-        {renderContent()}
+        <p className="text-2xl text-black-600 mt-2 font-bold p-5">Dashboard</p>
+        <div className="flex gap-4 p-6">
+          <div className="p-4 bg-neutral-200 rounded-lg shadow-lg flex-1 ">
+            <div className="flex items-center justify-between">
+              <IoBriefcaseOutline className="text-xl text-black-500" />
+            </div>
+            <p className="text-sm text-gray-600 mt-2 font-bold">
+              Total Jobs Completed
+            </p>
+            <div className="flex items-center justify-between">
+              <p className="text-2xl font-medium">20</p>
+            </div>
+          </div>
+          <div className="p-4 bg-neutral-200 rounded-lg shadow-lg flex-1 ">
+            <div className="flex items-center justify-between">
+              <LuClock className="text-xl text-black-500" />
+            </div>
+            <p className="text-sm text-gray-600 mt-2 font-bold">Pending Jobs</p>
+            <p className="text-2xl font-medium">2</p>
+          </div>
+          <div className="p-4 bg-neutral-200 rounded-lg shadow-lg flex-1 ">
+            <div className="flex items-center justify-between">
+              <CiBadgeDollar className="text-xl text-black-500" />
+            </div>
+            <p className="text-sm text-gray-600 mt-2 font-bold">Revenue</p>
+            <p className="text-2xl font-medium">$23,000</p>
+          </div>
+        </div>
+        <div className="p-6 bg-white rounded-lg shadow-md mt-4 border-solid border-2 border-gray-200">
+          <h2 className="text-xl font-semibold text-gray-700">
+            Client Activity
+          </h2>
+          <div className="mt-4">
+            <div className="flex justify-between">
+              <div>
+                <p className="text-2xl font-medium">2</p>
+                <p className="text-sm text-gray-600">New clients (this week)</p>
+              </div>
+              <div>
+                <p className="text-2xl font-medium">4</p>
+                <p className="text-sm text-gray-600">
+                  New clients (this month)
+                </p>
+              </div>
+            </div>
+            <div className="mt-4">
+              <div className="inline-block px-4 py-1 border border-black rounded-full text-sm font-sm text-gray-800 mb-4">
+                Recent Clients
+              </div>
+
+              <table className="w-full text-sm text-left rtl:text-right text-black-500">
+                <thead className="text-xs text-white  bg-blue-400 ">
+                  <tr>
+                    <th scope="col" className="px-6 py-3">
+                      Client ID
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Client Name
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Email
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Phone
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Status
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      #000
+                    </th>
+                    <td className="px-6 py-4">Jeff Richard</td>
+                    <td className="px-6 py-4">jeff101@email.com</td>
+                    <td className="px-6 py-4">(201)145-2453</td>
+                    <td className="px-6 py-4">Pending Quote</td>
+                  </tr>
+                  <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      #001
+                    </th>
+                    <td className="px-6 py-4">Jeff Richard</td>
+                    <td className="px-6 py-4">jeff101@email.com</td>
+                    <td className="px-6 py-4">(201)145-2453</td>
+                    <td className="px-6 py-4">Pending Quote</td>
+                  </tr>
+                  <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      #002
+                    </th>
+                    <td className="px-6 py-4">Jeff Richard</td>
+                    <td className="px-6 py-4">jeff101@email.com</td>
+                    <td className="px-6 py-4">(201)145-2453</td>
+                    <td className="px-6 py-4">Pending Quote</td>
+                  </tr>
+                  <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      #003
+                    </th>
+                    <td className="px-6 py-4">Jeff Richard</td>
+                    <td className="px-6 py-4">jeff101@email.com</td>
+                    <td className="px-6 py-4">(201)145-2453</td>
+                    <td className="px-6 py-4">Pending Quote</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   );
